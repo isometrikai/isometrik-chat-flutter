6c457e143705bda907964955adf09d4b96941a8e
RAHUL SARYAM <rahulsaryam@appscrip.co>
2025-04-03 14:51:42 +0530

    fix: rollback video trimer

--- lib/src/views/chat_page/widget/attachments/video_trimer.dart
+++ lib/src/views/chat_page/widget/attachments/video_trimer.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:isometrik_chat_flutter/isometrik_chat_flutter.dart';
-// import 'package:video_trimmer/video_trimmer.dart';
+import 'package:video_trimmer/video_trimmer.dart';
 
-// Todo refactor code
 class IsmVideoTrimmerView extends StatefulWidget {
   const IsmVideoTrimmerView({
     super.key,
@@ -22,44 +22,48 @@ class IsmVideoTrimmerView extends StatefulWidget {
 }
 
 class _VideoTrimmerViewState extends State<IsmVideoTrimmerView> {
-  // final Trimmer trimmer = Trimmer();
+  final Trimmer trimmer = Trimmer();
   var startValue = 0.0.obs;
   var endValue = 0.0.obs;
+  var durationInSeconds = 0.0;
   var isPlaying = false.obs;
   var playPausedAction = true;
   var descriptionTEC = TextEditingController();
+  final arguments = Get.arguments as Map<String, dynamic>? ?? {};
+  var file = XFile('');
 
   @override
   void initState() {
     super.initState();
-    endValue = widget.durationInSeconds.obs;
-
-    loadVideo();
+    endValue = (arguments['durationInSeconds'] as double? ?? 0).obs;
+    durationInSeconds = endValue.value;
+    file = arguments['file'] as XFile? ?? XFile('');
+    loadVideo(file.path);
   }
 
-  loadVideo() async {
-    // await trimmer.loadVideo(videoFile: widget.file);
-    // trimmer.videoPlayerController?.addListener(checkVideo);
+  loadVideo(String url) async {
+    await trimmer.loadVideo(videoFile: File(url));
+    trimmer.videoPlayerController?.addListener(checkVideo);
   }
 
   checkVideo() async {
-    // if (trimmer.videoPlayerController?.value.isPlaying == false) {
-    //   setState(() {
-    //     playPausedAction = true;
-    //   });
-    // }
+    if (trimmer.videoPlayerController?.value.isPlaying == false) {
+      setState(() {
+        playPausedAction = true;
+      });
+    }
   }
 
   Future<void> saveTrimVideo(double startValue, double endValue) async {
-    // await trimmer.saveTrimmedVideo(
-    //   startValue: startValue,
-    //   endValue: endValue,
-    //   onSave: (value) {
-    //     if (value != null) {
-    //       Get.back<File>(result: File(value));
-    //     }
-    //   },
-    // );
+    await trimmer.saveTrimmedVideo(
+      startValue: startValue,
+      endValue: endValue,
+      onSave: (value) {
+        if (value != null) {
+          Get.back<XFile>(result: XFile(value));
+        }
+      },
+    );
   }
 
   @override
@@ -69,7 +73,7 @@ class _VideoTrimmerViewState extends State<IsmVideoTrimmerView> {
         appBar: AppBar(
           leading: IconButton(
             onPressed: () {
-              Get.back<File>(result: widget.file);
+              Get.back<XFile>(result: file);
             },
             icon: const Icon(
               Icons.arrow_back_rounded,
@@ -96,74 +100,73 @@ class _VideoTrimmerViewState extends State<IsmVideoTrimmerView> {
           children: [
             IsmChatTapHandler(
               onTap: () async {
-                // var playBackState = await trimmer.videoPlaybackControl(
-                //   startValue: startValue.value,
-                //   endValue: endValue.value,
-                // );
-                // isPlaying.value = playBackState;
-                // playPausedAction = true;
-                // setState(() {});
-                // if (playBackState == false) return;
-                // await Future<void>.delayed(const Duration(milliseconds: 1000));
-                // playPausedAction = false;
-                // setState(() {});
+                var playBackState = await trimmer.videoPlaybackControl(
+                  startValue: startValue.value,
+                  endValue: endValue.value,
+                );
+                isPlaying.value = playBackState;
+                playPausedAction = true;
+                setState(() {});
+                if (playBackState == false) return;
+                await Future<void>.delayed(const Duration(milliseconds: 1000));
+                playPausedAction = false;
+                setState(() {});
               },
-              child: const Stack(
+              child: Stack(
                 alignment: Alignment.center,
                 children: [
-                  // AspectRatio(
-                  //   aspectRatio:
-                  //       trimmer.videoPlayerController?.value.aspectRatio ?? 0,
-                  //   child: VideoViewer(
-                  //     trimmer: trimmer,
-                  //   ),
-                  // ),
-                  // AnimatedOpacity(
-                  //   duration: const Duration(milliseconds: 250),
-                  //   opacity: playPausedAction ? 1 : 0,
-                  //   child:
-                  //       trimmer.videoPlayerController?.value.isPlaying == true
-                  //           ? Icon(
-                  //               Icons.pause_circle_rounded,
-                  //               color: IsmChatColors.whiteColor,
-                  //               size: IsmChatDimens.sixty,
-                  //             )
-                  //           : Icon(
-                  //               Icons.play_arrow_rounded,
-                  //               color: IsmChatColors.whiteColor,
-                  //               size: IsmChatDimens.sixty,
-                  //             ),
-                  // ),
+                  AspectRatio(
+                    aspectRatio:
+                        trimmer.videoPlayerController?.value.aspectRatio ?? 0,
+                    child: VideoViewer(
+                      trimmer: trimmer,
+                    ),
+                  ),
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 250),
+                    opacity: playPausedAction ? 1 : 0,
+                    child:
+                        trimmer.videoPlayerController?.value.isPlaying == true
+                            ? Icon(
+                                Icons.pause_circle_rounded,
+                                color: IsmChatColors.whiteColor,
+                                size: IsmChatDimens.sixty,
+                              )
+                            : Icon(
+                                Icons.play_arrow_rounded,
+                                color: IsmChatColors.whiteColor,
+                                size: IsmChatDimens.sixty,
+                              ),
+                  ),
                 ],
               ),
             ),
             Align(
               alignment: Alignment.topCenter,
               child: Padding(
-                  padding: IsmChatDimens.edgeInsetsTop10
-                      .copyWith(top: IsmChatDimens.hundred + IsmChatDimens.ten),
-                  child: const Text('Comming soone')
-                  // SizedBox(
-                  //   width: IsmChatDimens.percentWidth(.95),
-                  //   child: TrimViewer(
-                  //     showDuration: true,
-                  //     durationStyle: DurationStyle.FORMAT_MM_SS,
-                  //     trimmer: trimmer,
-                  //     viewerWidth: IsmChatDimens.percentWidth(.95),
-                  //     maxVideoLength:
-                  //         Duration(seconds: widget.durationInSeconds.toInt()),
-                  //     onChangeStart: (value) {
-                  //       startValue.value = value;
-                  //     },
-                  //     onChangeEnd: (value) {
-                  //       endValue.value = value;
-                  //     },
-                  //     onChangePlaybackState: (value) {
-                  //       isPlaying.value = false;
-                  //     },
-                  //   ),
-                  // ),
+                padding: IsmChatDimens.edgeInsetsTop10
+                    .copyWith(top: IsmChatDimens.hundred + IsmChatDimens.ten),
+                child: SizedBox(
+                  width: IsmChatDimens.percentWidth(.95),
+                  child: TrimViewer(
+                    showDuration: true,
+                    durationStyle: DurationStyle.FORMAT_MM_SS,
+                    trimmer: trimmer,
+                    viewerWidth: IsmChatDimens.percentWidth(.95),
+                    maxVideoLength:
+                        Duration(seconds: durationInSeconds.toInt()),
+                    onChangeStart: (value) {
+                      startValue.value = value;
+                    },
+                    onChangeEnd: (value) {
+                      endValue.value = value;
+                    },
+                    onChangePlaybackState: (value) {
+                      isPlaying.value = false;
+                    },
                   ),
+                ),
+              ),
             ),
           ],
         ),
