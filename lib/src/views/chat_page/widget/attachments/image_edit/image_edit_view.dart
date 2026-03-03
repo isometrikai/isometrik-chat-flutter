95ff6e7c08470179f9a468abfd851c799a098eec
Rasika Bharati <rasika@appscrip.co>
2026-03-03 20:13:17 +0530

    fix : 1.UI layout changes unexpectedly when keyboard opens after capturing image in chat, The screen alignment, spacing, and image preview layout appear different compared to the initial capture preview screen. 2. while sending the media when we tried to send long text The Ui is totally messed up

--- lib/src/views/chat_page/widget/attachments/image_edit/image_edit_view.dart
+++ lib/src/views/chat_page/widget/attachments/image_edit/image_edit_view.dart
@@ -15,6 +15,7 @@ class IsmChatImageEditView extends StatelessWidget {
         },
         builder: (controller) => Scaffold(
           backgroundColor: IsmChatColors.blackColor,
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             backgroundColor: IsmChatConfig.chatTheme.primaryColor,
             title: Text(
@@ -57,69 +58,94 @@ class IsmChatImageEditView extends StatelessWidget {
               onPressed: IsmChatRoute.goBack,
             ),
           ),
-          body: Image.memory(
-            controller.webMedia.first.platformFile.bytes ?? Uint8List(0),
-            fit: BoxFit.contain,
-            height: IsmChatDimens.percentHeight(1),
-            width: IsmChatDimens.percentWidth(1),
-            alignment: Alignment.center,
-          ),
-          floatingActionButton: Padding(
-            padding: IsmChatDimens.edgeInsetsLeft10
-                .copyWith(left: IsmChatDimens.thirty),
-            child: Row(
-              children: [
-                Expanded(
-                  child: IsmChatInputField(
-                    fillColor: IsmChatColors.greyColor,
-                    autofocus: false,
-                    padding: IsmChatDimens.edgeInsets0,
-                    hint: IsmChatStrings.addCaption,
-                    hintStyle: IsmChatStyles.w400White16,
-                    cursorColor: IsmChatColors.whiteColor,
-                    style: IsmChatStyles.w400White16,
-                    controller: controller.textEditingController,
-                    onChanged: (value) {
-                      controller.webMedia.first.caption = value;
-                    },
-                  ),
+          body: Stack(
+            children: [
+              Positioned.fill(
+                child: Image.memory(
+                  controller.webMedia.first.platformFile.bytes ?? Uint8List(0),
+                  fit: BoxFit.contain,
+                  alignment: Alignment.center,
                 ),
-                IsmChatDimens.boxWidth8,
-                FloatingActionButton(
-                  backgroundColor: IsmChatConfig.chatTheme.primaryColor,
-                  onPressed: () async {
-                    if (controller.webMedia.first.dataSize.size()) {
-                      IsmChatRoute.goBack();
-                      if (await IsmChatProperties.chatPageProperties
-                              .messageAllowedConfig?.isMessgeAllowed
-                              ?.call(context, controller.conversation,
-                                  IsmChatCustomMessageType.image) ??
-                          true) {
-                        await controller.sendImage(
-                          conversationId:
-                              controller.conversation?.conversationId ?? '',
-                          userId: controller
-                                  .conversation?.opponentDetails?.userId ??
-                              '',
-                          webMediaModel: controller.webMedia.first,
-                        );
-                      }
-                    } else {
-                      await IsmChatContextWidget.showDialogContext(
-                        content: const IsmChatAlertDialogBox(
-                          title: IsmChatStrings.youCanNotSend,
-                          cancelLabel: IsmChatStrings.okay,
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: IsmChatDimens.thirty,
+                    right: IsmChatDimens.ten,
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                    top: IsmChatDimens.ten,
+                  ),
+                  color: IsmChatColors.blackColor.withOpacity(0.8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ConstrainedBox(
+                          constraints: const BoxConstraints(
+                            maxHeight: 150.0,
+                          ),
+                          child: Scrollbar(
+                            child: IsmChatInputField(
+                              fillColor: IsmChatColors.greyColor,
+                              autofocus: false,
+                              padding: IsmChatDimens.edgeInsets0,
+                              hint: IsmChatStrings.addCaption,
+                              hintStyle: IsmChatStyles.w400White16,
+                              cursorColor: IsmChatColors.whiteColor,
+                              style: IsmChatStyles.w400White16,
+                              controller: controller.textEditingController,
+                              maxLines: null,
+                              minLines: 1,
+                              textInputAction: TextInputAction.newline,
+                              onChanged: (value) {
+                                controller.webMedia.first.caption = value;
+                              },
+                            ),
+                          ),
                         ),
-                      );
-                    }
-                  },
-                  child: const Icon(
-                    Icons.send,
-                    color: IsmChatColors.whiteColor,
+                      ),
+                      IsmChatDimens.boxWidth8,
+                      FloatingActionButton(
+                        backgroundColor: IsmChatConfig.chatTheme.primaryColor,
+                        onPressed: () async {
+                          if (controller.webMedia.first.dataSize.size()) {
+                            IsmChatRoute.goBack();
+                            if (await IsmChatProperties.chatPageProperties
+                                    .messageAllowedConfig?.isMessgeAllowed
+                                    ?.call(context, controller.conversation,
+                                        IsmChatCustomMessageType.image) ??
+                                true) {
+                              await controller.sendImage(
+                                conversationId:
+                                    controller.conversation?.conversationId ??
+                                        '',
+                                userId: controller.conversation?.opponentDetails
+                                        ?.userId ??
+                                    '',
+                                webMediaModel: controller.webMedia.first,
+                              );
+                            }
+                          } else {
+                            await IsmChatContextWidget.showDialogContext(
+                              content: const IsmChatAlertDialogBox(
+                                title: IsmChatStrings.youCanNotSend,
+                                cancelLabel: IsmChatStrings.okay,
+                              ),
+                            );
+                          }
+                        },
+                        child: const Icon(
+                          Icons.send,
+                          color: IsmChatColors.whiteColor,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
